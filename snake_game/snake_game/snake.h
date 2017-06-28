3d414cbe74cda325a6218898a4a148cdf4e2ea35
Karolis Jankauskas <Karolis Jankauskas>
2017-06-28 15:11:37 +0100

    Refactor the code for primitives

--- snake_game/snake_game/snake.h
+++ snake_game/snake_game/snake.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <algorithm>
 #include <vector>
 
 
@@ -10,13 +9,9 @@ namespace Snake
 
 	struct Snake
 	{
-		Snake() : head('x'), body('o'), segments(3), direction_y(0), direction_x(1), direction(GOING_RIGHT)
-		{
+		Snake() : head('x'), body('o'), segments(3), direction_y(0), direction_x(1), direction(GOING_RIGHT) {}
 
-		}
-
-		void SetUp(int max_steps, int world_width, int world_height, 
-			int segments = 3, int direction_x = 1, int direction_y = 0, int direction = GOING_RIGHT)
+		void SetUp(int max_steps, int world_width, int world_height)
 		{
 			steps_left_cpy = max_steps;
 			segments_cpy = segments;
@@ -24,27 +19,71 @@ namespace Snake
 			direction_y_cpy = direction_y;
 			direction_cpy = direction;
 
+			height = world_height;
+			width = world_width;
+
+			X = std::vector<int>(world_width * world_height, 0);
+			Y = std::vector<int>(world_width * world_height, 0);
+
 			for (int segment = 0; segment < segments; ++segment) {
-				X[segment] = X.size() / 2 - segment;
-				Y[segment] = Y.size() / 2;
+				X[segment] = width / 2 - segment;
+				Y[segment] = height / 2;
 			}
 		}
 
 		void Spawn()
 		{
+			food_eaten = 0;
 			steps_left = steps_left_cpy;
 			segments = segments_cpy;
 			direction_x = direction_x_cpy;
 			direction_y = direction_y_cpy;
 			direction = direction_cpy;
 
-			X = std::vector<int>(X.size());
-			Y = std::vector<int>(Y.size());
+			X = std::vector<int>(X.size(), 0);
+			Y = std::vector<int>(Y.size(), 0);
 
-			for (int segment = 0; segment < segments; ++segment) {
-				X[segment] = X.size() / 2 - segment;
-				Y[segment] = Y.size() / 2;
+			for (int s = 0; s < segments; ++s) {
+				X[s] = width / 2 - s;
+				Y[s] = height / 2;
 			}
+		}
+
+		void Move()
+		{
+			for (int s = segments; s > 0; --s) {
+				Y[s] = Y[s - 1];
+				X[s] = X[s - 1];
+			}
+
+			X[0] += direction_x;
+			Y[0] += direction_y;
+		}
+
+		bool IsAlive()
+		{
+			if (steps_left <= 0)
+				return false;
+
+			for (int s = segments; s > 0; --s)
+				if (X[s] == X[0] && Y[s] == Y[0])
+					return false;
+
+			if (X[0] > X.size())
+				return false;
+			if (X[0] < 1)
+				return false;
+			if (Y[0] > Y.size())
+				return false;
+			if (Y[0] < 1)
+				return false;
+
+			return true;
+		}
+
+		void ResetSteps()
+		{
+			steps_left = steps_left_cpy;
 		}
 
 		Snake &operator++() //for turning right
@@ -99,6 +138,8 @@ namespace Snake
 		int direction_y_cpy;
 		int direction_cpy;
 		int segments_cpy;
+		int width;
+		int height;
 	};
 
 	extern Snake snake;
